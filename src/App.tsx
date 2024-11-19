44fd9a52c7072db23671fa164fd10c1754a4cfa0
campbell-frost <campbellsfrost@gmail.com>
2024-11-18 22:28:11 -0500

    init imp

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,92 @@
+import { useState } from "react";
 import ToggleTheme from "./components/ToggleTheme";
+import { FolderOpen, FolderClosed, File } from "lucide-react";
 
 const root: Folder[] = [
   {
     label: "Home",
-    icon: "home",
-    children: []
+    children: [
+      {
+        label: "Documents",
+        children: []
+      },
+      {
+        label: "Pictures",
+        children: []
+      }
+    ]
   },
   {
     label: "Settings",
-    icon: "settings",
     children: [
       {
         label: "Profile",
-        icon: "user",
-        children: []
+        children: [],
       },
+      {
+        label: "Appearance",
+        children: [],
+      },
+      {
+        label: "Security",
+        children: [
+          {
+            label: "Password",
+            children: []
+          },
+          {
+            label: "2FA",
+            children: []
+          }
+        ]
+      }
     ]
+  },
+  {
+    label: "Help",
+    children: []
   }
-
-]
+];
 
 type Folder = {
   label: string;
-  icon?: string;
   children?: Folder[];
 }
 
-function App() {
+function Folder({ folder }: { folder: Folder }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="bg-background flex h-screen justify-center text-foreground items-center m-0 p-0">
-      <div className="absolute top-0 right-0 m-3 ">
-        <ToggleTheme />
+    <div className="gap-y-2 flex flex-col items-start">
+      <div className="flex items-center">
+        {folder.children?.length === 0 ? <File /> : isOpen ? <FolderOpen /> : <FolderClosed />}
+        <button className="ml-2" onClick={() => setIsOpen(!isOpen)}>
+          {folder.label}
+        </button>
       </div>
-      <h1>hi</h1>
+      {isOpen && folder.children?.map(child => (
+        <div className="ml-4">
+          <Folder folder={child} />
+        </div>
+      ))}
     </div>
   )
 }
 
-export default App
+function App() {
+  const [folder, setFolder] = useState(root);
+  return (
+    <div className="bg-background flex-col flex h-screen justify-center text-foreground items-center">
+      <div className="absolute top-0 right-0 m-3 ">
+        <ToggleTheme />
+      </div>
+      <div className="border-2 border-foreground rounded-md p-3 flex flex-col gap-y-2">
+        {folder.map(folder => (
+          <Folder folder={folder} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App;
