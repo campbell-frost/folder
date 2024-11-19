67011da169cdc10dc5fb143267230f32e05cf5da
campbell-frost <campbellsfrost@gmail.com>
2024-11-18 23:00:31 -0500

    tw

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,67 @@ import { FolderOpen, FolderClosed, File } from "lucide-react";
 
 const root: Folder[] = [
   {
-    label: "Home",
+    label: "src",
     children: [
       {
-        label: "Documents",
-        children: []
-      },
-      {
-        label: "Pictures",
-        children: []
-      }
-    ]
-  },
-  {
-    label: "Settings",
-    children: [
-      {
-        label: "Profile",
-        children: [],
-      },
-      {
-        label: "Appearance",
-        children: [],
-      },
-      {
-        label: "Security",
+        label: "components",
         children: [
           {
-            label: "Password",
-            children: []
-          },
-          {
-            label: "2FA",
-            children: []
+            label: "ToggleTheme.tsx",
           }
         ]
+      },
+      {
+        label: "hooks",
+        children: [
+          {
+            label: "Theme.tsx",
+          }
+        ]
+      },
+      {
+        label: "App.tsx",
+      },
+      {
+        label: "index.css",
+      },
+      {
+        label: "main.tsx",
+      },
+      {
+        label: "vite-env.d.ts",
       }
     ]
   },
   {
-    label: "Help",
-    children: []
+    label: ".gitignore",
+  },
+  {
+    label: "eslint.config.js",
+  },
+  {
+    label: "index.html",
+  },
+  {
+    label: "package-lock.json",
+  },
+  {
+    label: "package.json",
+  },
+  {
+    label: "postcss.config.js",
+  },
+  {
+    label: "README.md",
+  },
+  {
+    label: "tailwind.config.js",
+  },
+  {
+    label: "tsconfig.json",
+  },
+  {
+    label: "vite.config.ts",
   }
 ];
 
@@ -59,10 +79,12 @@ function Folder({ folder }: { folder: Folder }) {
   return (
     <div className="gap-y-2 flex flex-col items-start">
       <div className="flex items-center">
-        {folder.children?.length === 0 ? <File /> : isOpen ? <FolderOpen /> : <FolderClosed />}
-        <button className="ml-2" onClick={() => setIsOpen(!isOpen)}>
-          {folder.label}
-        </button>
+        <div className="flex items-center rounded px-2 py-1 hover:bg-muted cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+          {!folder.children ? <File className="icon-sm" /> : isOpen ? <FolderOpen className="icon-sm" /> : <FolderClosed className="icon-sm" />}
+          <p className="ml-2 group-hover:text-primary">
+            {folder.label}
+          </p>
+        </div>
       </div>
       {isOpen && folder.children?.map(child => (
         <div className="ml-4">
@@ -74,13 +96,13 @@ function Folder({ folder }: { folder: Folder }) {
 }
 
 function App() {
-  const [folder, setFolder] = useState(root);
+  const [folder, _] = useState(root);
   return (
-    <div className="bg-background flex-col flex h-screen justify-center text-foreground items-center">
+    <div className="bg-background flex-col flex h-screen justify-center text-foreground items-start">
       <div className="absolute top-0 right-0 m-3 ">
         <ToggleTheme />
       </div>
-      <div className="border-2 border-foreground rounded-md p-3 flex flex-col gap-y-2">
+      <div className=" p-3 flex flex-col gap-y-2 w-60 h-full border-r-2">
         {folder.map(folder => (
           <Folder folder={folder} />
         ))}
