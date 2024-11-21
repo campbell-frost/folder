cfcae30c2d45630df04c12d1f86d2578a9c7cc12
campbell-frost <campbellsfrost@gmail.com>
2024-11-21 18:00:53 -0500

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -5,53 +5,9 @@ import { type Folder, file } from "./assets/file";
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
 
-function Item({
-  children,
-  className,
-  isOpen,
-  isFile
-}: {
-  children: React.ReactNode,
-  className?: string,
-  isOpen?: boolean,
-  isFile?: boolean
-}) {
-  return (
-    <div className="flex items-center">
-      {isFile ? <File /> : isOpen ? <FolderOpen /> : <FolderClosed />}
-      <h1 className={twMerge("ml-2 group-hover:text-secondary", className)}>{children}</h1>
-    </div>
-  );
-}
-
-function Folder({ folder, onSelect, selectedId }: {
-  folder: Folder,
-  onSelect: (label: string) => void,
-  selectedId: string | null
-}) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className="flex flex-col justify-center gap-y-2 items-start w-full">
-      <div className={twMerge("group flex items-center hover:bg-muted cursor-pointer w-full px-2 py-1 rounded", selectedId === folder.label ? "bg-muted" : "")}
-        onClick={() => {
-          setIsOpen(!isOpen);
-          onSelect(folder.label);
-        }}>
-        <Item isOpen={isOpen} isFile={!folder.children} children={folder.label} />
-      </div>
-      {isOpen && folder.children?.map(child => (
-        <div key={child.label} className="ml-4 w-[calc(100%-1rem)]">
-          <Folder folder={child} onSelect={onSelect} selectedId={selectedId} />
-        </div>
-      ))}
-    </div>
-  )
-}
-
 function App() {
   const [folder, _] = useState(file);
-  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
   return (
     <div className="bg-background flex-col flex h-screen justify-center text-foreground items-start">
@@ -63,13 +19,56 @@ function App() {
           <Folder
             key={folder.label}
             folder={folder}
-            onSelect={setSelectedId}
-            selectedId={selectedId}
+            onSelect={setSelectedItem}
+            selectedItem={selectedItem}
           />
         ))}
       </div>
     </div>
   )
+}
+
+type FolderProps = {
+  folder: Folder,
+  selectedItem: string | null
+  onSelect: (label: string) => void,
+}
+
+function Folder({ folder, selectedItem, onSelect }: FolderProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col justify-center gap-y-2 items-start w-full">
+      <div className={twMerge("group flex items-center hover:bg-muted cursor-pointer w-full px-2 py-1 rounded", selectedItem === folder.label ? "bg-muted" : "")}
+        onClick={() => {
+          setIsOpen(!isOpen);
+          onSelect(folder.label);
+        }}>
+        <Item isOpen={isOpen} isFile={!folder.children} children={folder.label} />
+      </div>
+      {isOpen && folder.children?.map(child => (
+        <div key={child.label} className="ml-4 w-[calc(100%-1rem)]">
+          <Folder folder={child} onSelect={onSelect} selectedItem={selectedItem} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+type ItemProps = {
+  children: React.ReactNode,
+  className?: string,
+  isOpen?: boolean,
+  isFile?: boolean
+}
+
+function Item({ children, className, isOpen, isFile }: ItemProps) {
+  return (
+    <div className="flex items-center">
+      {isFile ? <File /> : isOpen ? <FolderOpen /> : <FolderClosed />}
+      <h1 className={twMerge("ml-2 group-hover:text-secondary", className)}>{children}</h1>
+    </div>
+  );
 }
 
 export default App;
